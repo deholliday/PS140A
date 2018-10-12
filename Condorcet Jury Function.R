284d82f7f175ad17fa5a96c482679bddc2a2fdb9
Derek Holliday <38087292+deholliday@users.noreply.github.com>
2018-10-12 13:05:31 -0700

    DH added cjr

---
+++ Condorcet Jury Function.R
@@ -0,0 +1,7 @@
+condorcet_jury <- function(N = 13, p = 0.5, m = floor(N/2)+1) {
+  results <- rep(NA,length(seq(m,N,1)))
+  for(i in seq(m,N,1)){
+    results[i-(m-1)] <- (factorial(N)/(factorial(N-i)*factorial(i)))*(p^i)*((1-p)^(N-i))
+  }
+  return(sum(results))
+}
