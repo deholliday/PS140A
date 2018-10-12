bf71aaab85e7609b32aa69732f44679f70a38190
Derek Holliday <38087292+deholliday@users.noreply.github.com>
2018-10-12 13:11:44 -0700

    DH updated cjf

--- Condorcet Jury Function.R
+++ Condorcet Jury Function.R
@@ -1,7 +1,11 @@
 condorcet_jury <- function(N = 13, p = 0.5, m = floor(N/2)+1) {
-  results <- rep(NA,length(seq(m,N,1)))
-  for(i in seq(m,N,1)){
-    results[i-(m-1)] <- (factorial(N)/(factorial(N-i)*factorial(i)))*(p^i)*((1-p)^(N-i))
+  if(N %% 2 == 0) {
+    return("ERROR: N must be odd to calculate majority")
+  } else {
+    results <- rep(NA,length(seq(m,N,1)))
+    for(i in seq(m,N,1)){
+      results[i-(m-1)] <- (factorial(N)/(factorial(N-i)*factorial(i)))*(p^i)*((1-p)^(N-i))
+    }
+    return(sum(results))
   }
-  return(sum(results))
 }
